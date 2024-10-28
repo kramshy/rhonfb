560d63348cfc30d4fa54b35c94e5b879cb77028c
Rhon Juztine Roqueno <162780080+kramshy@users.noreply.github.com>
2024-10-28 17:43:05 +0800

    qwq
    qqwqqqw

--- user/views.py
+++ user/views.py
@@ -11,7 +11,7 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned.data.get('username')
+            username = form.cleaned_data.get('username')
             messages.success(request, f'Accounnt created successfully for {username}.')
             return redirect('register')
     else:
